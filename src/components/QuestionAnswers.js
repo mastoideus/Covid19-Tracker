d2a38ee53df3a64ac9fe0b97d1900b05dedb5bd6
AZ <anelzubcevic@gmail.com>
2021-08-24 13:03:29 +0200

    second commit

--- src/components/QuestionAnswers.js
+++ src/components/QuestionAnswers.js
@@ -5,10 +5,10 @@ import {Card} from '@material-ui/core'
 import Question from './Question'
 
 const questionData= [
-    { id: 'q1a1', question:'Is the Covid-19 Vaccine Astra-Zeneca aproved?', answer:'The Covid-19 Vaccine Astra-Zeneca is well tested and not more dangerous then Vaccines from other medicine producer.'},
-    { id: 'q2a2', question:'Is the Covid-19 Vaccine Astra-Zeneca aproved?', answer:'The Covid-19 Vaccine Astra-Zeneca is well tested and not more dangerous then Vaccines from other medicine producer.'},
-    { id: 'q3a3', question:'Is the Covid-19 Vaccine Astra-Zeneca aproved?', answer:'The Covid-19 Vaccine Astra-Zeneca is well tested and not more dangerous then Vaccines from other medicine producer.'},
-    { id: 'q4a4', question:'Is the Covid-19 Vaccine Astra-Zeneca aproved?', answer:'The Covid-19 Vaccine Astra-Zeneca is well tested and not more dangerous then Vaccines from other medicine producer.'},
+    { id: 'q1a1', question:'IS COVID-19 WORSE THAN FLU?', answer:'COVID-19 has a higher rate of severe disease and mortality in nearly every age group, compared with influenza (flu).'},
+    { id: 'q2a2', question:'HOW CAN WE TRUST VACCINES THAT HAVE BEEN DEVELOPED SO FAST?', answer:'So far, all approved COVID-19 vaccines have undergone large clinical trials with rigorous safety protocols, and without major safety concerns raised.'},
+    { id: 'q3a3', question:'WHAT ARE THE RISKS OF RE-INFECTION?', answer:'Even if you have had a test that detects antibodies, reinfection is still possible, although less likely.'},
+    { id: 'q4a4', question:'IS REGULARLY WASHING MY HANDS ENOUGH TO PROTECT ME?', answer:'Even though regularly washing your hands is very important, as with masks it is not enough on its own.'},
     
 ]
 
